af82f1c4a9d8e42a91d2db9220c451a4b999b4b1
joaoo27 <tentakuloz69@gmail.com>
2026-08-04 09:25:36 -0300

    Add files via upload

---
+++ area do triangulo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+	
+	
+	float area, base, altura;
+	
+      base = 10;
+      altura = 20;
+      area = (base*altura)/2;
+	
+	printf("A area do triangulo (%.2f) * (%.2f) = %.2f", base,altura,area);
+	
+	
+	return 0;
+}
