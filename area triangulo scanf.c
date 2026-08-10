2b9bb1e02b1ab8ad20952aec0289fbb008326801
joaoo27 <tentakuloz69@gmail.com>
2026-08-10 09:32:02 -0300

    Add files via upload

---
+++ area triangulo scanf.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+	
+int base, altura, area;
+
+printf("insira o valor da base");
+scanf("%d", &base);
+
+printf("insira o valor da altura");
+scanf("%d", &altura);
+
+area = (base * altura)/2;
+
+printf("%d", area);
+	return 0;
+}
